d521a26bf0689f76803861dc49b7275e79fcedb0
jafetmendozac <abrahamjoelmc@gmail.com>
2025-12-20 11:51:45 -0500

    feat: fiberase emulator

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from './app/firebase';
 import { Button, Typography, Box } from '@mui/material'
-import { useEffect, useState } from "react";
-import Login from './auth/LoginPage';
-import Register from "./auth/RegisterPage";
-
 
 function App() {
 
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => unsub();
-  }, []);
-
-  if (!user) return <Login />;
 
   return (
     <>
